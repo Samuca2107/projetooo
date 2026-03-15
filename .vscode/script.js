50a429a25eaf0d9914400e45faeff9ff38d0b16e
Adriana V M Oliveira <ooliveira.samuca@gmail.com>
2026-03-15 17:44:23 -0300

    initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,25 @@
+function toggleMode(){
+const html= document.documentElement
+html.classList.toggle('light')
+// if(html.classList.contains ('light')){
+//     html.classList.remove('light')
+// }else{
+//     html.classList.add('light')
+// }
+
+const Img = document.querySelector ("#profile img")
+
+if( html.classList.contains('light')){
+    Img.setAttribute('src','./assets/avatar-samuel-light.png')
+}else{
+    Img.setAttribute('src','./assets/Avatar.samuel.dark.png')
+}
+
+if(html.classList.contains('light')){
+    Img.setAttribute('alt','foto de um cara com óculos de sol')
+}else{
+    Img.setAttribute('alt','foto de um cara com óculos de grau')
+}
+
+
+}
