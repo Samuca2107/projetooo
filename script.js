e43f556b5fec50e1f2574fea552c2fab65556bb6
Adriana V M Oliveira <ooliveira.samuca@gmail.com>
2026-03-19 23:46:53 -0300

    atualizações java script

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const Img = document.querySelector ("#profile img")
 if( html.classList.contains('light')){
     Img.setAttribute('src','./assets/avatar-samuel-light.png')
 }else{
-    Img.setAttribute('src','./assets/Avatar.samuel.dark.png')
+    Img.setAttribute('src','./assets/avatar.samuel.dark.png')
 }
 
 if(html.classList.contains('light')){
